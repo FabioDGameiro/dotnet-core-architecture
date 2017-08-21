681e664eee861ccc28b9f4dd61b6943cfb8a5996
Tiago Santos <tiagosantos@outlook.com>
2017-08-21 03:17:10 -0300

    jo

--- provas-de-conceito/restful-api/UsuariosAPI/Mappings/Profiles/UsuariosProfile.cs
+++ provas-de-conceito/restful-api/UsuariosAPI/Mappings/Profiles/UsuariosProfile.cs
@@ -16,13 +16,14 @@ namespace UsuariosAPI.Mappings.Profiles
         public UsuariosProfile()
         {
             // Entity -> Model
+
             CreateMap<Usuario, UsuarioGetModel>()
                 // Formatando o Nome Completo do Usuário a partir das propriedades Nome e Sobrenome
                 .ForMember(t => t.NomeCompleto, opt => opt.MapFrom(s => $"{s.Nome} {s.Sobrenome}"))
                 // Formatando a Idade do usuário a partir da sua Data de Nascimento
                 .ForMember(t => t.Idade, opt => opt.MapFrom(s => s.DataNascimento.GetCurrentAge()))
                 // Formatando o Sexo do usuário a partir do enum SexoType
-                .ForMember(t => t.Sexo, opt => opt.MapFrom(s => (s.Sexo.HasValue) ? s.Sexo.Value.GetDescription() : string.Empty));
+                .ForMember(t => t.Sexo, opt => opt.MapFrom(s => (s.Sexo.HasValue) ? s.Sexo.Value.GetDescription() : null));
 
             CreateMap<UsuarioEndereco, UsuarioEnderecoGetModel>()
                 // Formatando o Tipo do endereçoa partir do enum EnderecoType
