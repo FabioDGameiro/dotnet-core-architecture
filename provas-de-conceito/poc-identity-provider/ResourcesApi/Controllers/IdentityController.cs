94b1553f4ed6e1039baa0ac2fddcb9f118cebcb8
Tiago Santos <taigobrasil@gmail.com>
2017-10-02 10:42:58 -0300

    Adição de uma API com o controller seguro e um ConsoleApp que simula um javascript client

---
+++ provas-de-conceito/poc-identity-provider/ResourcesApi/Controllers/IdentityController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ResourcesApi.Controllers
+{
+    [Authorize]
+    [Route("identity")]
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
