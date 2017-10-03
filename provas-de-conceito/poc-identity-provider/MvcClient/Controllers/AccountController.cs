3b8cf2dce6dcbd5073d0f5f46e36fd5b71c3af88
Tiago Santos <taigobrasil@gmail.com>
2017-10-03 17:27:42 -0300

    resstruturação e refatoração da solução

---
+++ provas-de-conceito/poc-identity-provider/MvcClient/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Project.MvcApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            if (!Url.IsLocalUrl(returnUrl)) returnUrl = "/";
+
+            var props = new AuthenticationProperties
+            {
+                RedirectUri = returnUrl
+            };
+
+            return Challenge(props, "oidc");
+        }
+
+
+        [Authorize]
+        public IActionResult Secure()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> GetApiClaims()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.SetBearerToken(accessToken);
+            var content = await client.GetStringAsync("http://localhost:3000/identity");
+
+            ViewBag.Json = JArray.Parse(content).ToString();
+            return View();
+        }
+
+        public IActionResult Denied(string returnUrl = null)
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
+    }
+}
