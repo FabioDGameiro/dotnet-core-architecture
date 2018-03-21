405e7fcae5045164789063b805819c480c3d6150
Tiago Santos <taigobrasil@gmail.com>
2018-03-21 15:47:51 -0300

    e globalization delicia

---
+++ provas-de-conceito/poc-api-globalization-localization/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IStringLocalizer _localizer;
+
+        public ValuesController(IStringLocalizerFactory factory)
+        {
+            var type = typeof(MessagesResource);
+            var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+            _localizer = factory.Create("MessagesResource", assemblyName.Name);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new
+            {
+                CurrentCulture = CultureInfo.CurrentCulture.NativeName,
+                CurrentUICulture = CultureInfo.CurrentUICulture.NativeName,
+                Date = DateTime.Now.ToString("D"),
+                Number = (1234567.89).ToString("n"),
+                Currency = (4452.24).ToString("C"),
+                BundaLocalized = _localizer["Hello"].Value
+            };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ provas-de-conceito/poc-api-globalization-localization/WebApi/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRequestLocalization(BuildLocalizationOptions());
+
+            app.UseMvc();
+        }
+
+        private RequestLocalizationOptions BuildLocalizationOptions()
+        {
+            var supportedCultures = new List<CultureInfo>
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("pt-BR")
+            };
+
+            var options = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            };
+
+            return options;
+        }
+    }
+}
